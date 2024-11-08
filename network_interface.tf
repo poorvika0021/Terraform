9ffa6b9562d86cf6ea755d50c520779923f35154
Ubuntu <TerraformPoo@TerraformPoo.acxbmu3wlqsepeuznoan0mjd0b.xx.internal.cloudapp.net>
2024-11-08 07:49:00 +0000

    adding network_interface.tf and public_ip.tf files

---
+++ network_interface.tf
@@ -0,0 +1,45 @@
+resource "azurerm_resource_group" "rg" {
+  name     = var.rg_name
+  location = var.rg_location
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  location            = var.rg_location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = var.address_space
+}
+
+resource "azurerm_subnet" "snet" {
+  name                 = var.snet_name
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = var.subnet_space
+}
+
+resource "azurerm_ssh_public_key" "ssh" {
+  name                = var.ssh_key
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.rg_location
+  public_key          = file("~/.ssh/id_rsa.pub")
+}
+
+resource "azurerm_public_ip" "pip" {
+  name                = var.public_ip_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.rg_location
+  allocation_method   = "Static"
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = var.nic_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.rg_location
+  ip_configuration {
+    name                          = var.ip_name
+    private_ip_address_allocation = "Dynamic"
+    subnet_id                     = azurerm_subnet.snet.id
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
+}
+
